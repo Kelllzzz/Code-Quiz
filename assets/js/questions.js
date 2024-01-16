88f0d7152f03b1a640ad843dce8cb3abd7de40de
Kelllzzz <kellakhigbe@gmail.com>
2024-01-16 23:55:49 +0000

    created quiz questions in the questions js file.

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var quizData = [
+    {
+        Question:"1. Commonly used data types DO NOT include" ,
+        options:["Strings","Booleans","Alerts","Numbers"],
+        answer: "Alerts",
+    },
+    {
+        Question:"2. String values must be enclosed within ____________ when being assigned to variables" ,
+        options:["Commas","Curly brackets","Quotes","Parenthesis"],
+        answer: "Quotes",
+    },
+    {
+        Question:"3. Arrays in JavaScript can be used to store" ,
+        options:["Numbers & Strings","Other arrays","Booleans","All of the above"],
+        answer: "All of the above",
+    },
+    {
+        Question:"4. A Condition in an if/Else statement is enclosed within" ,
+        options:["Quotes","Curly brackets","Parenthesis","Square brackets"],
+        answer: "Parenthesis",
+    },
+    {
+        Question:"5. A very useful tool used during development & debugging for printing content to the debugger is " ,
+        options:["Javascript","Terminal / bash","For loops","Console.log"],
+        answer: "Console.log",
+    },
+]
