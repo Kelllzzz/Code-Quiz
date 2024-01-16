797a09f2faf0e0387cf59a4a1242c418e6706fd8
Kelllzzz <kellakhigbe@gmail.com>
2024-01-16 23:58:52 +0000

    added function to start quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,6 +8,30 @@ var currentIdx = 0;
 var endScreen = document.getElementById("end-screen")
 var endScore = document.getElementById('final-score')
 
+//function to start the quiz
+function startProcess() {
+    console.log("Start button was clicked!");
+    questions.classList.remove("hide")
+    startPage.classList.add("hide")
+//     displayQuestionAndOptions();
+}
+var button = document.getElementById("start");
+button.addEventListener("click", startProcess, false);
+
+var countdown;
+var count = 75; // Sets the countdown time
+
+document.getElementById("start").addEventListener("click", function () {
+    countdown = setInterval(function () {
+        count--;
+        document.getElementById("time").innerText = count;
+
+        if (count <= 0) {
+            endQuiz()
+            document.getElementById("time").innerText = "You're out of Time!";
+        }
+    }, 1000);
+});
 
 
 
