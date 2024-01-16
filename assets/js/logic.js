ab24dfa5002124d79de2bc825526d7a2d5e0a299
Kelllzzz <kellakhigbe@gmail.com>
2024-01-16 23:51:24 +0000

    assigning variables

---
+++ assets/js/logic.js
@@ -0,0 +1,19 @@
+// assigning variables
+var questions = document.getElementById("questions");
+var startPage = document.getElementById("start-screen");
+var timer = document.getElementById('time');
+var choices = document.getElementById('choices')
+var score = 0;
+var currentIdx = 0;
+var endScreen = document.getElementById("end-screen")
+var endScore = document.getElementById('final-score')
+
+
+
+
+
+
+
+
+
+
